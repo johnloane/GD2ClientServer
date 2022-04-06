9792c1767000fdf470abea1d70a9aa71a3a75274
John Loane <john.loane@dkit.ie>
2022-04-06 11:46:55 +0100

    UDP Combo Service
    There is no guaranteed delivery here. Dangerous!

--- src/main/java/com/dkit/gd2/johnloane/comboserviceclient/ComboServiceClient.java
+++ src/main/java/com/dkit/gd2/johnloane/comboserviceclient/ComboServiceClient.java
@@ -1,8 +1,10 @@
 package com.dkit.gd2.johnloane.comboserviceclient;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ComboServiceClient
@@ -70,9 +72,69 @@ public class ComboServiceClient
                     System.out.println("Message sent");
 
                     //Wait for and process the response....
-                    //This is your homework....
+                    //Create space for the message
+                    byte[] responseBuffer = new byte[MAX_LEN];
+                    DatagramPacket responsePacket = new DatagramPacket(responseBuffer, responseBuffer.length);
+                    //Wait to receive the response
+                    clientSocket.receive(responsePacket);
+                    //Get the data out of the packet
+                    String data = new String(responsePacket.getData(), responsePacket.getOffset(), responsePacket.getLength());
+                    System.out.println("Response: " + data.trim() + ".");
                 }
+                System.out.println();
+            }
+            System.out.println("Thank you for using the Combo Service App");
+            Thread.sleep(3000);
+        }
+        catch(IOException ioe)
+        {
+            System.out.println(ioe.getMessage());
+        }
+        catch(InterruptedException ie)
+        {
+            System.out.println(ie.getMessage());
+        }
+        finally
+        {
+            if(clientSocket != null)
+            {
+                clientSocket.close();
             }
         }
+    }
+
+    private static Protocol getChoice(Scanner keyboard)
+    {
+        Protocol choice = Protocol.END;
+        boolean validChoice = false;
+        while(!validChoice)
+        {
+            try
+            {
+                int input = keyboard.nextInt();
+                choice = Protocol.values()[input];
+                validChoice = true;
+            }
+            catch(InputMismatchException ime)
+            {
+                System.out.println("Please enter a valid menu option");
+                System.out.println();
+                displayMenu();
+            }
+            finally
+            {
+                keyboard.nextLine();
+            }
+        }
+        return choice;
+    }
+
+    private static void displayMenu()
+    {
+        System.out.println("Please choose one of the following options: ");
+        System.out.println("1) To echo a message");
+        System.out.println("2) To get current date and time");
+        System.out.println("3) View the number of requests that the server has serviced");
+        System.out.println("0) To quit the program");
     }
 }
--- src/main/java/com/dkit/gd2/johnloane/comboserviceclient/Protocol.java
+++ src/main/java/com/dkit/gd2/johnloane/comboserviceclient/Protocol.java
@@ -3,7 +3,6 @@ package com.dkit.gd2.johnloane.comboserviceclient;
 public enum Protocol
 {
     END,
-    NONE,
     ECHO,
     DAYTIME,
     STATS
